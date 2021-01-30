d5eb6e53c6b5e5b368c347e0253767c97dd860fb
Anthony Egwu <tony5egwu@gmail.com>
2021-01-30 20:15:43 +0100

    Initial Commit

---
+++ Nightswatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nightswatch
+//
+//  Created by Anthony Egwu on 30/01/2021.
+//  Copyright © 2021 Anthony Egwu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
